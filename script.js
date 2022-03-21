abcc2f13d96dc8a8fb0a2e391529b2ed06e7907b
emicif <emanuela.cifolelli@gmail.com>
2022-03-21 15:17:49 +0100

    init

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
 console.log('JS OK!');
+
+/* Aggiungere un file js in cui definire *un array di oggetti* che rappresentano i membri del team. 
+// Ogni membro ha le informazioni necessarie per stampare la relativa card: *Nome, Ruolo e Foto*.
+Una volta definita la struttura dati, prendendo come riferimento la card di esempio presente nell'html, stampare dinamicamente una card per ogni membro del team.*/
+
+// creare array di oggetto -> membri del team
+// team informazioni: Nome, ruolo e foto
+
+
+
+// creare array di oggetto -> membri del team
+
+//const team = ['Angela Caroll', 'Walter Gordon', 'Angela Lopez', 'Scott Estrada', 'Barbara Ramos'];
+
+//const ruolo = ['Founder & Ceo', 'Chief Editor', 'Office Manager', 'Social Media Manager', 'Developer', 'Graphic Designer'];
+
+const team = [
+    {
+        nome: 'Angela Caroll',
+        ruolo: 'Founder & Ceo'
+    },
+    {
+        nome: 'Wlater Gordon',
+        ruolo: 'Chief Editor'
+    }
+];
+// console.log(team.nome); //se fosse solo object
+
+// console.log([0]) // mi stampa 0
+
+// console.log(team[0]); // stampa il primo gruppo di graffe
+
+console.log((team.nome)[0] + (team.nome)); // stampare solo il primo nome
+
+
+
